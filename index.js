88da8de2eea91da56043084ba5b6ea1dcef0c947
islammazharul <islammazharul3645@gmail.com>
2023-05-03 01:21:15 +0600

    install cors policy

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors')
 
+app.use(cors())
 const categories = require('./data/categories.json');
 const recipes = require('./data/recipes.json');
 
